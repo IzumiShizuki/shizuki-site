1cb4df079f5f3f491c8cd9ba9a8191d6772cab94
Izumi <29301481@qq.com>
2026-02-08 21:18:15 +0800

    init : spring初始化

---
+++ src/main/java/io/github/shizuki/site/ShizukiSiteApplication.java
@@ -0,0 +1,13 @@
+package io.github.shizuki.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShizukiSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShizukiSiteApplication.class, args);
+	}
+
+}
